88c86982189f4622f903cb2f458e7136d3b1b3f5
amankumar89 <amankumaroo784@gmail.com>
2024-08-02 10:15:43 +0530

    added products routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from 'express'; 
 import dotenv from 'dotenv';
+import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  return res.send("Hello from product store server");
-})
+// products 
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, (error) => {
   if (error) return console.log('Error in running server', error);
